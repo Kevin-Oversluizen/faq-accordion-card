dc3d14abbe2cac46c6f9ae31276c107796a33968
Kevin-Oversluizen <94692567+Kevin-Oversluizen@users.noreply.github.com>
2023-10-11 09:55:18 +0200

    Style updates and minor changes

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-// "use strict";
-
-// const buttonAccordion = document.querySelectorAll(".js-accordion-toggle");
-
-// buttonAccordion.forEach((button) => {
-//   button.addEventListener("click", () => {
-//     const parentAccordionItem = button.parentNode;
-//     const content = parentAccordionItem.querySelector(".accordion_content");
-
-//     // If the clicked button already has the class, remove it and return
-//     if (button.classList.contains("accordion__toggle--openend")) {
-//       button.classList.remove("accordion__toggle--openend");
-//       content.classList.remove("accordion_content-opened");
-//       return;
-//     }
-
-//     // Remove classes from all buttons and their corresponding content
-//     buttonAccordion.forEach((otherButton) => {
-//       otherButton.classList.remove("accordion__toggle--openend");
-//       const otherParentAccordionItem = otherButton.parentNode;
-//       const otherContent =
-//         otherParentAccordionItem.querySelector(".accordion_content");
-//       otherContent.classList.remove("accordion_content-opened");
-//     });
-
-//     // Add classes to the clicked button and its corresponding content
-//     button.classList.add("accordion__toggle--openend");
-//     content.classList.add("accordion_content-opened");
-//   });
-// });
-
 "use strict";
 
 const buttonAccordion = document.querySelectorAll(".js-accordion-toggle");
